1e70b5fd52c1797d1103fefc411cbc8e117f78a6
darwinx <39998019+dwn-24@users.noreply.github.com>
2023-07-19 20:21:44 -0500

    Update

---
+++ src/main/java/ec/edu/espe/arquitectura/mongo/examen/model/EmpresaUno.java
@@ -0,0 +1,18 @@
+package ec.edu.espe.arquitectura.mongo.examen.model;
+
+public class EmpresaUno {
+    private String ruc;
+    private String razonSocial;
+    private String cuentaPrincipal;
+    private String empleados;
+    private String ci;
+    private String cedula;
+    private String apellidos;
+    private String nombres;
+    private String numeroCuenta;
+
+
+    
+
+    
+}
